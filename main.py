50cbcf025a48d2422f916dfef4537c85cee79b34
morelon231 <morelon231@unicaen.fr>
2025-02-06 15:54:51 +0100

    Changement de nom de l'app

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import streamlit as st
 
-st.title('My App')
+st.title('InvestMate')
 st.write("Bienvenue sur ma plateforme dédiée à l'analyse des actions et à l'évaluation du prix juste d'une action en bourse. Ce site vous permet d'explorer les performances financières d'une société, ainsi que d'évaluer son Fair Price en fonction de ses flux de trésorerie futurs.")
 
 pages = {"Stock screener": [st.Page("screener_python.py")], "Evaluation du prix juste": [st.Page("fair_price.py")]}
