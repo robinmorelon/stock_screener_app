8de48782211e7357dd5833eab077722191c1614b
morelon231 <morelon231@unicaen.fr>
2025-02-12 11:10:46 +0100

    ajout warning page accueil

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import streamlit as st
 
 st.title('InvestMate')
 st.write("Bienvenue sur ma plateforme dédiée à l'analyse des actions et à l'évaluation du prix juste d'une action en bourse. Ce site vous permet d'explorer les performances financières d'une société, ainsi que d'évaluer son Fair Price en fonction de ses flux de trésorerie futurs.")
+st.write(":warning: Investir comporte des risques de pertes en capital, vous restez responsable de vos investissements.")
 
 pages = {"Stock screener": [st.Page("screener_python.py")], "Evaluation du prix juste": [st.Page("fair_price.py")]}
 pg = st.navigation(pages)
